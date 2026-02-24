e36f6595aaac48f3edde89aa5a9831155fa0dac7
heriknavdiwala763-ctrl <heriknavdiwala763@gmail.com>
2026-02-24 22:10:55 +0530

    Update ass_1026_13.c

--- ass_1026_13.c
+++ ass_1026_13.c
@@ -1,75 +1,79 @@
-#include <stdio.h>
-struct date
+#include<stdio.h>
+struct hotel
 {
-    int day;
-    int month;
-    int year;
+    char name[20];
+    char address[30];
+    int grade;
+    float average_room_charge;
+    int no_of_rooms;
 };
-int leap_year(int year)
-{
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-        return 1;
-    else
-        return 0;
-}
-int update_date(struct date *d)
-{
-    int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    if (d->month == 2 && leap_year(d->year))
-    {
-        days_in_month[2] = 29;
-    }
-
-    d->day++;
-
-    if (d->day > days_in_month[d->month])
-    {
-        d->day = 1;
-        d->month++;
-
-        if (d->month > 12)
-        {
-            d->month = 1;
-            d->year++;
-        }
-    }
-
-    return 1;
-}
-int new_date(struct date *d, int days)
-{
-    int i;
-    for (i = 0; i < days; i++)
-    {
-        update_date(d);
-    }
-    return 1;
-}
-
-void printDate(struct date d)
-{
-    char *month_names[] = {
-        "", "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"};
-
-    printf("%s %d, %d\n", month_names[d.month], d.day, d.year);
-}
-
+void displaybygrade(struct hotel h[],int n,int grade);
+void displaybycharge(struct hotel h[],int n,float charge);
+void sortbycharge(struct hotel h[],int n);
 int main()
 {
-    struct date d, updated_date;
-    int days;
-
-    printf("Enter day, month and year: ");
-    scanf("%d %d %d", &d.day, &d.month, &d.year);
-
-    printf("Enter number of days to add: ");
-    scanf("%d", &days);
-
-    new_date(&d, days);
-    printf("Next date is:\n");
-    printDate(d);
-
+    struct hotel h[5];
+    int i,grade;
+    float charge;
+    for(i=0;i<5;i++)
+    {
+        printf("enter details of hotel %d\n",i+1);
+        printf("name: ");
+        scanf("%s",h[i].name);
+        printf("address: ");
+        scanf("%s",h[i].address);
+        getchar();
+        printf("grade: ");
+        scanf("%d",&h[i].grade);
+        printf("average room charge: ");
+        scanf("%f",&h[i].average_room_charge);
+        printf("number of rooms: ");
+        scanf("%d",&h[i].no_of_rooms);
+    }
+    printf("enter grade to display hotels :: ");
+    scanf("%d",&grade);
+    displaybygrade(h,5,grade);
     return 0;
+}
+void displaybygrade(struct hotel *h,int n,int grade)
+{
+    int i;
+    sortbycharge(h,n);
+    printf("hotels with grade %d:\n",grade);
+    for(i=0;i<n;i++)
+    {
+        if(h[i].grade==grade)
+        {
+            printf("name: %s\naddress: %s\n",h[i].name,h[i].address);
+        }
+    }
+}
+void displaybycharge(struct hotel *h,int n,float charge)
+{
+    int i;
+    printf("hotels with room charge less than %.2f\n",charge);
+    for(i=0;i<n;i++)
+    {
+        if(h[i].average_room_charge<charge)
+        {
+            printf("%s %.2f\n",h[i].name,h[i].average_room_charge);
+        }
+    }
+}
+void sortbycharge(struct hotel *h,int n)
+{
+    int i,j;
+    struct hotel temp;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(h[j].average_room_charge>h[j+1].average_room_charge)
+            {
+                temp=h[j];
+                h[j]=h[j+1];
+                h[j+1]=temp;
+            }
+        }
+    }
 }
